e8edfa9e5fe9694cfb78585940c49d06ee4c9f78
larissa429 <105134537+larissa429@users.noreply.github.com>
2026-08-02 14:16:42 -0500

    creds

--- components.js
+++ components.js
@@ -21,7 +21,7 @@
 /* the small, fixed set of non-character utility pages. Anything else
    one level deep is assumed to be a character page — so new
    character folders NEVER need to be added here manually again. */
-var UTILITY_FOLDERS = ["about", "residents", "audition", "calendar", "resources", "art", "guestbook"];
+var UTILITY_FOLDERS = ["about", "residents", "audition", "calendar", "resources", "art", "guestbook", "credits"];
 
 var pathParts = location.pathname.split("/").filter(Boolean);
 var here = pathParts[pathParts.length - 1] || "";
